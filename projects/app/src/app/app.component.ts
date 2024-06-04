edc5508bfab8afa3af947bf196ffc0ab508d6275
Augusto Prodohl <51026238+xLonelyPlayer@users.noreply.github.com>
2024-06-03 21:37:04 -0300

    Modified demo page
    Modified components versions

--- projects/app/src/app/app.component.ts
+++ projects/app/src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { DemoComponent } from './components/demo/demo.component';
+
 import { NgxAspnButtonComponent } from 'button';
 import { NgxAspnInputComponent } from 'input';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgxAspnButtonComponent, NgxAspnInputComponent],
+  imports: [
+    RouterOutlet,
+    NgxAspnButtonComponent,
+    NgxAspnInputComponent,
+    DemoComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
